7d63ff5b7ed9fa19b5ec58ac875e26736574f3f1
Badri <programmingwithbadri@outlook.com>
2020-07-13 14:26:24 +0530

    Add the status to each posts

--- index.js
+++ index.js
@@ -16,10 +16,10 @@ const handleEvent = (type, data) => {
   }
 
   if (type === "CommentCreated") {
-    const { id, content, postId } = data;
+    const { id, content, postId, status } = data;
 
     const post = posts[postId];
-    post.comments.push({ id, content });
+    post.comments.push({ id, content, status });
   }
 };
 
