e3ccb34668abcaa5df24436b987c2b24584255f6
Badri <programmingwithbadri@outlook.com>
2020-07-13 15:38:04 +0530

    Sync the service with event bus when the service is down

--- index.js
+++ index.js
@@ -51,4 +51,13 @@ app.post("/events", (req, res) => {
 
 app.listen(4002, async () => {
   console.log("Listening on 4002");
+
+  // Once the service is up, get all the events from the Event bus
+  const res = await axios.get('http://localhost:4005/events');
+
+  for (let event of res.data) {
+    console.log('Processing event:', event.type);
+
+    handleEvent(event.type, event.data);
+  }
 });
