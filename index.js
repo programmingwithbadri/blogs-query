aeb0c530604086fbd8a684da2c1ab95807198921
Badri <programmingwithbadri@outlook.com>
2020-07-13 13:39:55 +0530

    Parsing the incoming comments from event bus

--- index.js
+++ index.js
@@ -6,14 +6,35 @@ const app = express();
 app.use(bodyParser.json());
 app.use(cors());
 
+const posts = {};
+
+// Handle the event
+const handleEvent = (type, data) => {
+  if (type === "PostCreated") {
+    const { id, title } = data;
+    posts[id] = { id, title, comments: [] };
+  }
+
+  if (type === "CommentCreated") {
+    const { id, content, postId } = data;
+
+    const post = posts[postId];
+    post.comments.push({ id, content });
+  }
+};
+
 // Send the posts
 app.get("/posts", (req, res) => {
-
+  res.send(posts);
 });
 
 // Receive the events from Event bus
 app.post("/events", (req, res) => {
-  
+  const { type, data } = req.body;
+
+  // Handle the received event from Event bus
+  handleEvent(type, data);
+  res.send({});
 });
 
 app.listen(4002, async () => {
